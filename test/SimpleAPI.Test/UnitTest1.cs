55fbbd78662bce71dd112548a9cfed8610ccf739
Andrzej Kociuba <fastandy@o2.pl>
2019-09-08 14:47:19 +0200

    init files

---
+++ test/SimpleAPI.Test/UnitTest1.cs
@@ -0,0 +1,19 @@
+using SimpleAPI.Controllers;
+using System;
+using Xunit;
+
+namespace SimpleAPI.Test
+{
+    public class UnitTest1
+    {
+        ValuesController controller = new ValuesController();
+
+
+        [Fact]
+        public void GetValues()
+        {
+            var returnValue = controller.Get(1);
+            Assert.Equal("Something", returnValue);
+        }
+    }
+}
